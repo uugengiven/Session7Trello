245860a50f5e99ca2272786eb242ce58936ea8c2
John Lange <lange.john.m@gmail.com>
2018-09-27 10:57:17 -0400

    Initial Commit

---
+++ Session7Trello/Controllers/CardsController.cs
@@ -0,0 +1,35 @@
+﻿using Session7Trello.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Session7Trello.Controllers
+{
+    public class CardsController : Controller
+    {
+        // GET: Cards
+        public ActionResult Index()
+        {
+            NoteDbContext notesdb = new NoteDbContext();
+            var darnita = notesdb.Categories.ToList();
+            ViewBag.Categories = darnita;
+            var blake = notesdb.Categories.ToList();
+            return View(blake);
+        }
+
+        public ActionResult Create(string Title, string Text, int Category)
+        {
+            NoteDbContext notesdb = new NoteDbContext();
+            Note silence = new Note();
+            silence.Title = Title;
+            silence.Text = Text;
+            silence.Category = notesdb.Categories.Find(Category);
+            notesdb.Notes.Add(silence);
+            notesdb.SaveChanges();
+
+            return View();
+        }
+    }
+}
---
+++ Session7Trello/Models/TrelloDbStuff.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace Session7Trello.Models
+{
+    public class Note
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Text { get; set; }
+        
+        public virtual Category Category { get; set; }
+    }
+
+    public class Category
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+
+        public virtual List<Note> Notes { get; set; }
+    }
+
+    public class NoteDbContext : DbContext
+    {
+        public DbSet<Note> Notes { get; set; }
+        public DbSet<Category> Categories { get; set; }
+    }
+}
