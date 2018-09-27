5f0dadfac695c49af41ba7c42f608d17c4129eb5
John Lange <lange.john.m@gmail.com>
2018-09-27 09:25:01 -0400

    Add project files.

---
+++ Session7Trello/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Session7Trello.Startup))]
+namespace Session7Trello
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
